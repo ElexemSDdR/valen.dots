ad542ecc71c982497c092ac8998b691500c49606
ElexemSDdR <valendomta@gmail.com>
2025-05-23 14:37:39 -0300

    putting the .desktop files

--- configs/apps/nvim/lua/config/keymaps_for_different_files/markdown.lua
+++ configs/apps/nvim/lua/config/keymaps_for_different_files/markdown.lua
@@ -1,8 +1,18 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.keymap.set("i", "<C-b>", "****", { buffer = true, desc = "Set four * to bold" })
-    vim.keymap.set("i", "<C-i>", "**", { buffer = true, desc = "Set two * to italize" })
+    vim.keymap.set("i", "<C-b>", function()
+      vim.api.nvim_feedkeys("****", "n", false)
+      vim.schedule(function()
+          vim.api.nvim_input("<Left><Left>")
+        end)
+end, { buffer = true, desc = "Set four * to bold" })
+    vim.keymap.set("i", "<C-i>", function()
+      vim.api.nvim_feedkeys("**", "n", false)
+      vim.schedule(function()
+          vim.api.nvim_input("<Left>")
+        end)
+end, { buffer = true, desc = "Set two * to italize" })
   end,
 })
 
---
+++ configs/apps/terminals/instructions.md
@@ -0,0 +1,8 @@
+# How to put the applications (put the .desktop files to get executable icons and else)
+
+open the **desktops_files** folder and copy the alacritty.desktop and kitty.desktop to the applications folder on your system
+
+```bash
+cp ./desktops_files/kitty.desktop /usr/share/applications/
+cp ./desktops_files/alacritty.desktop /usr/share/applications/
+```
