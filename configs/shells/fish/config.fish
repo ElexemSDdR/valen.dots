3a20c25669f2a38f76c802199671012bd2a2557f
ElexemSDdR <valendomta@gmail.com>
2025-06-17 01:10:05 -0300

    Fixing the fish configuration

--- configs/shells/fish/config.fish
+++ configs/shells/fish/config.fish
@@ -272,3 +272,5 @@ function fzf_key_bindings
 end
 ### end: key-bindings.fish ###
 fzf_key_bindings
+
+set -gx PATH "/run/user/1000/fnm_multishells/12778_1750133254224/bin" $PATH; set -gx FNM_MULTISHELL_PATH "/run/user/1000/fnm_multishells/12778_1750133254224"; set -gx FNM_VERSION_FILE_STRATEGY "local"; set -gx FNM_DIR "/home/valen/.local/share/fnm"; set -gx FNM_LOGLEVEL "info"; set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist"; set -gx FNM_COREPACK_ENABLED "false"; set -gx FNM_RESOLVE_ENGINES "true"; set -gx FNM_ARCH "x64";
